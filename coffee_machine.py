e00cf13cc56f04836afec39fe6a090f27bb29802
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-08 13:54:02 +0300

    added comments

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,29 +1,32 @@
 import os
-from menu import menu
+from menu import menu  # Importing the menu dictionary
 from art import logo
 
-# resources
+# Initial resources
 water = 300
 milk = 200
 coffee = 100
 money = 0
 
-completed = True
+# Flag for handling numeric input errors
 number_error_handler = True
 
 
+# Function to calculate the amount of money inserted by the user
 def calculate_money():
     global number_error_handler
-    while number_error_handler:
 
+    while number_error_handler:
         print("Please insert coins!")
         quarter = input("How many Quarters?: ")
         dime = input("How many dime?: ")
         nickle = input("How many nickle?: ")
         pennie = input("How many pennie?: ")
 
+        # Checking if all inputs are numeric
         if quarter.isnumeric() and dime.isnumeric() and nickle.isnumeric() and pennie.isnumeric():
             number_error_handler = False
+            # Calculating total amount inserted
             pay = 0.25 * int(quarter) + 0.10 * int(dime) + 0.05 * int(nickle) + 0.01 * int(pennie)
             return pay
         else:
@@ -31,17 +34,19 @@ def calculate_money():
             print("Insert numbers only!")
 
 
-while completed:
+# Main loop of the coffee machine
+while True:
     print(logo)
     order = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
     if order in menu:
         for item in menu:
             if order == item:
-
+                # Checking if resources are sufficient for the order
                 if water >= menu[item]["Water"] and coffee >= menu[item]["Coffee"]:
                     water -= menu[item]["Water"]
                     coffee -= menu[item]["Coffee"]
+                    # Handling payment
                     loop = True
                     while loop:
                         payment = calculate_money()
@@ -57,21 +62,25 @@ while completed:
                                 input("Tap any key to order!")
                                 os.system('cls')
                         else:
-
                             print("Please pay enough amount for the product!")
 
+    # Reporting resources and money earned
     elif order == "report":
         os.system('cls')
         print(f"Water: {water}ml")
         print(f"Milk: {milk}ml")
-        print(f"coffee: {coffee}g")
+        print(f"Coffee: {coffee}g")
         print(f"Money: ${money}")
+
+    # Turning off the machine
     elif order == "off":
-        completed = False
         os.system('cls')
         print("Machine in maintenance mode!")
+        break  # Exit the loop
+
     else:
         os.system('cls')
-        print("Please input a valid input!")
+        print("Please input a valid option!")
 
+# Program ends here
 input("Tap Enter to Exit!")
